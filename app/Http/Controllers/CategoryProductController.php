5798c64a49973e0370ba0c462903700de3e0d77d
vietthang1107 <nvt1999@gmail.com>
2021-03-26 22:33:16 +0700

    them danh muc va hien thi danh muc

---
+++ app/Http/Controllers/CategoryProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Session;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+session_start();
+
+class CategoryProductController extends Controller
+{
+    //
+    public function add_category_product(){
+        return view('admin.add_category_product');
+
+    }
+
+    public function all_category_product(){
+
+        $all_category_product = DB::table('tbl_category_product')->get();
+        $manager_category_product = view('admin.all_category_product')->with('all_category_product',$all_category_product);
+        return view('admin_layout')->with('admin.all_category_product',$manager_category_product);
+
+    }
+
+    public function save_category_product(Request $request){
+       $data = array();
+       $date['category_name'] = $request->category_product_name;
+       $date['category_desc'] = $request->category_product_desc;
+       $date['category_status'] = $request->category_product_status;
+
+       DB::table('tbl_category_product')->insert($date);
+       session::put('message','Thêm danh mục sản phẩm thành công');
+       return Redirect::to('add-category-product');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,23 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//frontend
 Route::get('/', [HomeController::class, 'index']);
+
 Route::get('/trang-chu','App\Http\Controllers\HomeController@index');
-   
+
+//backend
+Route::get('/admin','App\Http\Controllers\AdminController@index');
+
+Route::get('/dashboard','App\Http\Controllers\AdminController@show_dashboard');
+
+Route::get('/logout','App\Http\Controllers\AdminController@logout');
+
+Route::post('/admin-dashboard','App\Http\Controllers\AdminController@dashboard');
+
+//Category product
+Route::get('/add-category-product','App\Http\Controllers\CategoryProductController@add_category_product');
+
+Route::get('/all-category-product','App\Http\Controllers\CategoryProductController@all_category_product');
+
+Route::post('/save-category-product','App\Http\Controllers\CategoryProductController@save_category_product');
